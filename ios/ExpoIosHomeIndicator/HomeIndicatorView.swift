afccb2913f6eb6c6e3845c6a2c94efc40ad25e9c
Mohiuddin Ahmed <mohiuddin@tieredcommunication.com>
2022-06-06 15:02:48 +0600

    wip

--- ios/ExpoIosHomeIndicator/HomeIndicatorView.swift
+++ ios/ExpoIosHomeIndicator/HomeIndicatorView.swift
@@ -2,20 +2,20 @@ import UIKit
 import ExpoModulesCore
 
 @objc(ExpoIosHomeIndicatorView)
-public class HomeIndicatorView: ExpoView {
+public class ExpoIosHomeIndicatorView: ExpoView {
     private var prefersAutoHidden: Bool
 
     override var prefersHomeIndicatorAutoHidden: Bool {
         return true
     }
-    
+
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-            
+
         setNeedsUpdateOfHomeIndicatorAutoHidden()
     }
 
-  
+
     @objc
     public func setPrefersAutoHidden(_ autoHidden: Bool) {
         prefersAutoHidden = autoHidden
